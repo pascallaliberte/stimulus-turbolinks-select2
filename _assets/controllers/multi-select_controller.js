987cc277ab53881d6d4817ad00773498e519f4c8
Pascal Laliberté <pascal@pascallaliberte.me>
2018-02-01 17:07:31 -0500

    multi-select_controller: use linked target properties

--- _assets/controllers/multi-select_controller.js
+++ _assets/controllers/multi-select_controller.js
@@ -1,6 +1,8 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
+  static targets = [ "select" ]
+  
   connect() {
     this.select2mount()
     
@@ -10,28 +12,24 @@ export default class extends Controller {
   }
   
   select2mount() {
-    const placeholder = $(this.select).data('placeholder')
+    const placeholder = $(this.selectTarget).data('placeholder')
     
-    $(this.select).select2({
+    $(this.selectTarget).select2({
       placeholder: placeholder
     });
   }
   
   select2unmount() {
     this.saveState()
-    $(this.select).select2('destroy');
+    $(this.selectTarget).select2('destroy');
   }
   
   saveState() {
-    let values = $(this.select).val()
+    let values = $(this.selectTarget).val()
     
     // make sure the HTML itself has those elements selected, since the HTML is what is saved in the turbolinks snapshot
     values.forEach((val) => {
-      $(this.select).find(`option[value="${val}"]`).attr('selected', 'selected');
+      $(this.selectTarget).find(`option[value="${val}"]`).attr('selected', 'selected');
     })
-  }
-  
-  get select() {
-    return this.targets.find("select")
   }
 }
